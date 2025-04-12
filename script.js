b46b4f12471c2ef5dfce4d8eb16220091208444d
Bereket Million <alphaberk10@gmail.com>
2025-04-12 23:36:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.shop').hide();
+    }
+    $('.test').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.shop').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768){
+        $('.service').hide();
+    }
+    $('.services').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.service').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) { 
+    $('.account').hide();
+    }
+    $('.accounts').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.account').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.store').hide();
+    }
+    $('.stores').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.store').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.buisness').hide();
+    }
+    $('.buisnesses').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.buisness').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.education').hide();
+    }
+    $('.educations').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.education').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.healthcare').hide();
+    }
+    $('.healthcares').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.healthcare').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.government').hide();
+    }
+    $('.governments').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.government').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.value').hide();
+    }
+    $('.values').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.value').toggle(); // Toggle the content visibility
+        }
+    });
+});
+
+$(document).ready(function() {
+    if ($(window).width() < 768) {
+    $('.about').hide();
+    }
+    $('.abouts').click(function() {
+        if ($(window).width() < 768) { // Check if screen width is less than 768px
+            $('.about').toggle(); // Toggle the content visibility
+        }
+    });
+});
